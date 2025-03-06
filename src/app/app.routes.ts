0f627fc33c6c3cbff1690105836d3a637a8450eb
karlaguinaldodev <aguinaldokarl.al@gmail.com>
2025-03-06 13:47:02 +0800

    Added import of the events and create components and added the gabaygov logo for the navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { EventsComponent } from './events/events.component';
+import { CreateEventComponent } from './create-event/create-event.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'events',
+    component: EventsComponent
+  },
+  {
+    path: 'create-event',
+    component: CreateEventComponent
+  }
+];
