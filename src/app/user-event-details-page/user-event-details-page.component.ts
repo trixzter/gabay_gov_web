9f8b78465940ce37d7a4b68f1187bd182919f462
Angelo Untalan <angelountalan17@gmail.com>
2025-03-07 12:05:06 +0800

    created user event details page

---
+++ src/app/user-event-details-page/user-event-details-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user-event-details-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user-event-details-page.component.html',
+  styleUrl: './user-event-details-page.component.css'
+})
+export class UserEventDetailsPageComponent {
+  event = {
+    title: "Libreng Gupit Para Sa Kabataan",
+    date: "Monday, March 17, 2025 ",
+    time: "8:00 AM to 5:00 PM",
+    location: "tiaong",
+    about: "Tuloy tuloy lang ang Serbisyo at Suporta natin para sa mga Mag-aaral na KABATAAN na mag babalik Eskwela! Libreng GUPIT! handog sa inyo ng Sangguniang Kabataan! sa Sampalucan. Siguraduhing kasama ninyo ang inyong mga Magulang!",
+  };
+}
