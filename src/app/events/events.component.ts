787c10e55245d89003cec14b8a5ef65225d5d436
karlaguinaldodev <aguinaldokarl.al@gmail.com>
2025-03-10 10:36:05 +0800

    Seperated the time and date in interface and ts

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -15,14 +15,16 @@ export class EventsComponent {
     {
       id: 1,
       title: "Libreng Gupit Para Sa Kabataan",
-      date: "Monday, March 17, 2025, 8:00 AM to 5:00 PM",
+      date: "Monday, March 17, 2025",
+      time: "8:00 AM to 5:00 PM",
       location: "Tanza, Cavite, Basketball Court",
       imageUrl: "LIBRENG-GUPIT.jpg"
     },
     {
       id: 2,
       title: "Libreng Gupit In Partnership With Red Republic Salon",
-      date: "Wednesday, March 19, 2025, 8:00 AM to 5:00 PM",
+      date: "Wednesday, March 19, 2025",
+      time: "8:00 AM to 5:00 PM",
       location: "Silang, Cavite, Basketball Court",
       imageUrl: "LIBRENG-GUPIT-2.jpg"
     },
