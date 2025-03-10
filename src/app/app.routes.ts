5066e0f0a41e80ea334b325d8fcde632352cf313
karlaguinaldodev <aguinaldokarl.al@gmail.com>
2025-03-10 10:50:32 +0800

    Added a router link for home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { EventsComponent } from './events/events.component';
 import { CreateEventComponent } from './create-event/create-event.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'create-event',
     component: CreateEventComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 ];
