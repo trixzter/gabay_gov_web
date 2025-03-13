cfc163ca44138d9a10ae95ac1b01fa4774354551
karlaguinaldodev <aguinaldokarl.al@gmail.com>
2025-03-13 10:23:41 +0800

    Fixed the errors

--- src/app/user-event-details-page/user-event-details-page.component.ts
+++ src/app/user-event-details-page/user-event-details-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserNavigationHeaderComponent } from '../user-navigation-header/user-navigation-header.component';
-import { EventService } from '../services/event.service';
+import { EventService } from '../services/events/event.service';
 import { EventModel } from '../models/event.model';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
@@ -15,12 +15,15 @@ import { ActivatedRoute } from '@angular/router';
     FormsModule
   ],
   templateUrl: './user-event-details-page.component.html',
-  styleUrl: './user-event-details-page.component.scss'
+  styleUrl: './user-event-details-page.component.scss',
 })
 export class UserEventDetailsPageComponent implements OnInit {
-  event: EventModel = {} as EventModel;  
+  event: EventModel = {} as EventModel;
 
-  constructor(private eventService: EventService, private route: ActivatedRoute) {}
+  constructor(
+    private eventService: EventService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     const eventId = Number(this.route.snapshot.paramMap.get('id'));
@@ -28,7 +31,7 @@ export class UserEventDetailsPageComponent implements OnInit {
   }
 
   getEventDetails(eventId: number): void {
-    this.eventService.getEventById(eventId).subscribe((data) => {
+    this.eventService.getEvent(eventId).subscribe((data) => {
       if (data) {
         this.event = data;
       }
