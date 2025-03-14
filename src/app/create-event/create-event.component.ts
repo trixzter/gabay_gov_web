d3f36581379acd8d22ec26a41078f8cecafeeb2a
Jann Alexcis Mercado <jannmercado07@gmail.com>
2025-03-14 12:07:32 +0800

    trying to resolve conflict

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrganizerNavigationHeaderComponent } from '../organizer-navigation-header/organizer-navigation-header.component';
-import { EventModel } from '../models/event.model';
 import { EventService } from '../services/events/event.service';
+import { EventModel } from '../models/event.model';
 import { FormsModule } from '@angular/forms';
 import { Router} from '@angular/router';
 @Component({
