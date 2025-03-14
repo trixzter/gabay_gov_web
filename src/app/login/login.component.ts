09017ca142e5b8be0dba6fada0d12d47ba4fbc73
marc aron alfonso <marc.aron.alfonso21@gmail.com>
2025-03-14 09:51:26 +0800

    created login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../services/users/user.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+  imports: [FormsModule, CommonModule], 
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'] // Fixed property name
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private router: Router, private userService: UserService) {}
+
+  loginUser() {
+    const credentials = { username: this.username, password: this.password };
+
+    this.userService.login(credentials).subscribe({ // Changed from register() to login()
+      next: (res: any) => {
+        console.log('Login successful:', res);
+        if (res.token) {
+          localStorage.setItem('token', res.token);
+        }
+        alert('Login successful!');
+        this.router.navigate(['/events']);
+      },
+      error: (err) => {
+        console.error('Login failed:', err);
+        alert('Invalid username or password.');
+      }
+    });
+  }
 }
