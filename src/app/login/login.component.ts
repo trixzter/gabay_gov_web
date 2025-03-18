85207844d2f2482b535004b192ad9efc0abde742
Karl Angelo Aguinaldo <aguinaldokarl.al@gmail.com>
2025-03-18 13:16:01 +0800

    Added RouterModule in login-component, for create account to work when clicked

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../services/users/user.service';
@@ -11,6 +11,7 @@ import { UserModel } from '../models/user.model';
   imports: [
     FormsModule,
     CommonModule,
+    RouterModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
