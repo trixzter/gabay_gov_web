e298db200e3f9a693c8ef3ba361350a0e11af737
Karl Angelo Aguinaldo <aguinaldokarl.al@gmail.com>
2025-03-19 13:53:40 +0800

    Added search functionality in user events page and user nav header

--- src/app/user-navigation-header/user-navigation-header.component.ts
+++ src/app/user-navigation-header/user-navigation-header.component.ts
@@ -1,16 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-navigation-header',
   standalone: true,
   imports: [ 
     RouterLink, 
-    RouterModule
+    RouterModule,
+    CommonModule,
+    FormsModule
   ],
   templateUrl: './user-navigation-header.component.html',
   styleUrl: './user-navigation-header.component.scss'
 })
 export class UserNavigationHeaderComponent {
+  title: string = '';
+  location: string = '';
 
+  constructor(private router: Router) {}
+
+  onSearch(): void {
+    if (this.title || this.location) {
+      this.router.navigate(['/user-events'], {
+        queryParams: { title: this.title, location: this.location }
+      });
+    }
+  }
 }
